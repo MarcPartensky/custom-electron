ba7919b8992b6fffc45a9203feceec75ee96f1e9
Marc Partensky <marc.partensky@gmail.com>
2023-06-12 23:30:13 +0200

    best style possible

--- main.js
+++ main.js
@@ -1,8 +1,34 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, globalShortcut } = require('electron');
+
+let win;
+let zoom = 1.0; // Initial zoom factor
 
 function createWindow() {
-  const win = new BrowserWindow();
+  const win = new BrowserWindow({
+     autoHideMenuBar: true
+  });
   win.loadURL('http://localhost:8000'); // Replace with your server URL
+  win.webContents.setZoomFactor(10); // Adjust the value as needed
+
+  // Register global shortcuts for zooming in and out
+  globalShortcut.register('CommandOrControl+=', () => {
+    zoom += 0.1;
+    win.webContents.setZoomFactor(zoom);
+  });
+
+  globalShortcut.register('CommandOrControl+-', () => {
+    zoom -= 0.1;
+    win.webContents.setZoomFactor(zoom);
+  });
+
+  // Disable scroll behavior for the main window
+  win.webContents.on('did-finish-load', () => {
+    win.webContents.insertCSS(`
+      ::-webkit-scrollbar {
+        display: none;
+      }
+    `);
+  });
 }
 
 app.whenReady().then(() => {
